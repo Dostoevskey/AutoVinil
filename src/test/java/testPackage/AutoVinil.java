323cf15de78a3cc8c837fa7c584c11469e521820
Igor <dostoevskey@gmail.com>
2018-09-14 00:30:58 -0400

    Fixed assertions for all tests.

--- src/test/java/testPackage/AutoVinil.java
+++ src/test/java/testPackage/AutoVinil.java
@@ -21,72 +21,72 @@ public class AutoVinil {
         driver.get("http://autovinil76.ru");
     }
 
-    @AfterTest
-    public final void teardown() {
-        if (driver != null) {
-            driver.close();
-        }
-    }
-
     @Test
     public final void validateHeaderHome() {
-        Assert.assertTrue(driver.findElement(By.cssSelector("div.menu a[href='/']")).
-                getText().contains("Главная"), ("Главная is matching"));
-    }
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.menu a[href='/']")).
+                getText(),"Главная");
+            }
 
     @Test
     public final void validateHeaderServices() {
         driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/uslugi/']")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/uslugi/']")).
-                getText().contains("Услуги"), ("Услуги is matching"));
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/uslugi/']")).
+                getText(),"Услуги");
     }
 
     @Test
     public final void validateHeaderItems() {
         driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/tovari/']")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/tovari/']")).
-                getText().contains("Товары"), ("Товары is matching"));
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/tovari/']")).
+                getText(),"Товары");
     }
 
     @Test
     public final void validateHeaderPrices() {
         driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/ceni/']")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/ceni/']")).
-                getText().contains("Цены"), ("Цены is matching"));
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/ceni/']")).
+                getText(),"Цены");
     }
 
     @Test
     public final void validateHeaderSales() {
         driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/akcii/']")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/akcii/']")).
-                getText().contains("Акции"), ("Акции is matching"));
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/akcii/']")).
+                getText(),"Акции");
     }
 
     @Test
     public final void validateHeaderArticles() {
         driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/stati/']")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/stati/']")).
-                getText().contains("Статьи"), ("Статьи is matching"));
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/stati/']")).
+                getText(),"Статьи");
     }
 
     @Test
     public final void validateHeaderPortfolio() {
         driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/portfolio/']")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/portfolio/']")).
-                getText().contains("Портфолио"), ("Портфолио is matching"));
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/portfolio/']")).
+                getText(),"Портфолио");
     }
 
     @Test
     public final void validateHeaderAboutUs() {
         driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/o-nas/']")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/o-nas/']")).
-                getText().contains("О нас"), ("О нас is matching"));
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/o-nas/']")).
+                getText(),"О нас");
     }
 
     @Test
     public final void validateHeaderContacts() {
         driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/kontakti/']")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/kontakti/']")).
-                getText().contains("Контакты"), ("Контакты is matching"));
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.block1 a[href='http://autovinil76.ru/kontakti/']")).
+                getText(),"Контакты");
+    }
+
+    @AfterTest
+    public final void teardown() {
+        if (driver != null) {
+            driver.close();
+        }
     }
 }
