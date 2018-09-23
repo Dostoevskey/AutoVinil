1bf97aa2bfd521a2ccd8fbb670202f03486a5332
Igor <dostoevskey@gmail.com>
2018-09-23 06:17:17 -0400

    Created 'GosUslugiFileUpload' class.

--- src/test/java/testPackage/GosUslugiFileUpload.java
+++ src/test/java/testPackage/GosUslugiFileUpload.java
@@ -22,11 +22,22 @@ public class GosUslugiFileUpload {
 
     @Test
     public void chooseRegion() {
-        driver.findElement(By.xpath("//div[text()='Выбрать вручную']")).isDisplayed();
-        driver.findElement(By.xpath("//div[text()='Выбрать вручную']")).click();
-        driver.findElement(By.xpath("//label[text()='Название населенного пункта']")).isEnabled();
-        driver.findElement(By.xpath("//label[text()='Название населенного пункта']")).click();
-        driver.findElement(By.xpath("//label[text()='Название населенного пункта']").senDKeys
+        //driver.findElement(By.xpath("//div[text()='Выбрать вручную']")).isDisplayed();
+        driver.findElement(By.xpath("*//em[@class='wrapper w50']//span[@class='icon']")).isDisplayed();
+        driver.findElement(By.xpath("*//em[@class='wrapper w50']//span[@class='icon']")).click();
+//        driver.findElement(By.xpath("*//em[@class='wrapper w50']")).isDisplayed();
+//        driver.findElement(By.xpath("*//em[@class='wrapper w50']")).click();
+//        driver.findElement(By.xpath("//label[text()='Название населенного пункта']")).isDisplayed();
+//        driver.findElement(By.xpath("//label[text()='Название населенного пункта']")).click();
+        driver.findElement(By.xpath("//label[@ng-if]")).isDisplayed();
+        driver.findElement(By.xpath("//label[@ng-if]")).click();
+//        driver.findElement(By.xpath("//div[@class='select-el']")).isDisplayed();
+//        driver.findElement(By.xpath("//div[@class='select-el']")).click();
+
+
+//        driver.findElement(By.xpath("*//input[@type='search']")).click();
+
+        driver.findElement(By.xpath("//label[text()='Название населенного пункта']")).sendKeys("Москва");
 
 //        driver.switchTo().frame(driver.findElement());
 //        driver.switchTo(By.ByLinkText("Название населенного пункта"));
